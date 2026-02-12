8ea885b771a3daed355fa6c3a0e8e4699180e5cb
Emma <88465944+emijane@users.noreply.github.com>
2026-02-12 16:15:57 -0500

    added navigation title and style updates

--- clerk-nextjs/src/components/header/NavigationMenu.tsx
+++ clerk-nextjs/src/components/header/NavigationMenu.tsx
@@ -12,26 +12,31 @@ import {
 
 export function NavigationMenuComponent() {
     return (
-        <NavigationMenu>
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                        <Link href="/">Home</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
+        <div className="w-full flex justify-between">
+            <div>
+                <h1 className="text-2xl font-bold">Stacked</h1>
+            </div>
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                            <Link href="/">Home</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
 
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                        <Link href="http://localhost:3000/u/emi">Profile</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                            <Link href="http://localhost:3000/u/emi">Profile</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
 
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                        <Link href="http://localhost:3000/settings/profile">Settings</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                            <Link href="http://localhost:3000/settings/profile">Settings</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
     );
 }

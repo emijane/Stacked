d7f929febfae6b914a25e2e80e29f9e87734e5be
Emma <88465944+emijane@users.noreply.github.com>
2026-05-04 13:36:47 -0400

    removed all pages other than home page

--- clerk-nextjs/src/components/header/NavigationMenu.tsx
+++ clerk-nextjs/src/components/header/NavigationMenu.tsx
@@ -23,12 +23,6 @@ export function NavigationMenuComponent() {
                             <Link href="/">Home</Link>
                         </NavigationMenuLink>
                     </NavigationMenuItem>
-
-                    <NavigationMenuItem>
-                        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                            <Link href="/u/emi">Profile</Link>
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
                 </NavigationMenuList>
             </NavigationMenu>
         </div>
